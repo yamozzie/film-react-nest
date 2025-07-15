6636bc2b4114c661bd90a58dc124a9d24eb85532
Said Alistanov <alistanov.said@yandex.ru>
2025-07-15 23:34:52 +0300

    added eslint exception

--- backend/src/order/controller/order.controller.ts
+++ backend/src/order/controller/order.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { OrderService } from '../service/order.service';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { OrderDto } from '../dto/order.dto';
 
 @Controller('order')
